dd2177c8164f14ccb82979677dc1b09eb6879377
Ankit <ankit.roy@appzlogic.com>
2026-08-25 20:26:41 +0530

    Configure SQLite database

---
+++ node_modules/backend/src/db/database.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const databasePath = path.join(__dirname, '../../data/videoselz.db');
+
+const db = new sqlite3.Database(databasePath, (error) => {
+  if (error) {
+    console.error('Failed to connect to SQLite:', error.message);
+    return;
+  }
+
+  console.log('Connected to SQLite database.');
+});
+
+db.run('PRAGMA foreign_keys = ON');
+
+module.exports = db;
