b5e7e565c2ea1a0cd56085ffe66ac54361d9fec4
Ankit <ankit.roy@appzlogic.com>
2026-08-25 21:09:36 +0530

    chore: add repository exports

---
+++ backend/src/repositories/index.js
@@ -0,0 +1,9 @@
+const productRepository = require('./productRepository');
+const videoRepository = require('./videoRepository');
+const engagementEventRepository = require('./engagementEventRepository');
+
+module.exports = {
+  productRepository,
+  videoRepository,
+  engagementEventRepository
+};
